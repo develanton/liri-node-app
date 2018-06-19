c541d5b4ee4a1ddf4dca9baf0bd4a2ae01ef0596
Develanton <develanton@gmail.com>
2018-06-19 02:57:04 -0500

    all functionalities are working

--- homework_instructions.md
+++ homework_instructions.md
@@ -17,7 +17,7 @@ In this assignment, you will make LIRI. LIRI is like iPhone's SIRI. However, whi
    * [Node-Spotify-API](https://www.npmjs.com/package/node-spotify-api)
    
    * [Request](https://www.npmjs.com/package/request)
-
+-its
      * You'll use Request to grab data from the [OMDB API](http://www.omdbapi.com).
 
    * [DotEnv](https://www.npmjs.com/package/dotenv)
--- liri.js
+++ liri.js
@@ -1,5 +1,11 @@
 
 
+
+/*
+TODO:
+* Get default routes
+*/
+
 //Requirements
 require("dotenv").config();
 
@@ -7,6 +13,7 @@ var keys = require("./keys.js");
 var request = require("request");
 var Spotify = require('node-spotify-api');
 var Twitter = require("twitter");
+var fs = require("fs");
 
 
 
@@ -14,61 +21,137 @@ debugger;
 //Global Variables
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
-var condition = process.argv[2];
-var input = process.argv[3];
+var input = process.argv[2];
+var argument = "";
+var condition = "";
+var argumentArray = [];
+
+debugger;
+
+function main(condition, argument) {
+    debugger;
+
+    condition = input;
+    argument = thirdArgument();
+
+    debugger;
+
+    switch (condition) {
+        case "my-tweets":
+            tweets();
+            break;
+
+        case "spotify-this-song":
+
+            var song = argument;
+            debugger
+
+            if (song === "") {
+                defaultTrack();
+                debugger
+
+            } else {
+                spot();
+                debugger
+
+            }
+            debugger
+            break;
+
+        case "movie-this":
+
+            if (argument === "") {
+                movieDefault()
+            } else {
+                movie();
+                debugger
+            }
+            break;
+
+        case "do-what-it-says":
+            what();
+            debugger;
+            break;
 
 
-
-
-switch (condition) {
-    case "my-tweets":
-        tweets();
-        break;
-
-    case "spotify-this-song":
-        spot();
-        break;
-
-    case "movie-this":
-        movie();
-        break;
-
-    case "do-what-it-says":
-        what();
-        break;
-
-    default: 
-        check();
-        break;
+        default:
+            check();
+            break;
+    }
 }
 
+function tweets() {
 
-function tweets(){
-    client.get('statuses/user_timeline', function(error, tweets, response) {
-        if(error) throw error;
-        
-        
+    console.log('\n'+"Loading..."+'\n');
+
+    client.get('statuses/user_timeline', function (error, tweets, response) {
+        if (error) throw error;
+
+
         for (let index = 0; index < tweets.length; index++) {
-            console.log(tweets[index].text); 
-            
+            console.log(tweets[index].text);
+
         }
         // The favorites.
         //console.log(response);  // Raw response object.
-      });
+    });
 }
 
 function spot() {
-    console.log(spotify);
-    console.log(Spotify);
-    
+
+    console.log('\n'+"Loading..."+'\n');
+
+    spotify.search({ type: 'track', query: argument }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+
+        }
+
+        var myArr = data.tracks.items;
+        for (let i = 0; i < myArr.length; i++) {
+            console.log('\nArtist: ' + myArr[i].artists[0].name + '\n');
+            console.log('Track Name: ' + myArr[i].name + '\n');
+            console.log('Track Preview: ' + myArr[i].preview_url + '\n');
+            console.log('Album: ' + myArr[i].album.name + '\n');
+            console.log('---------------------------------------------------------------------------------------------------------------------\n')
+            debugger
+        }
+    });
+    debugger
+
 }
 
+function defaultTrack() {
 
+    console.log('\n'+"Loading..."+'\n');
+
+    spotify.search({ type: 'track', query: "The Sign ace of base" }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+
+        }
+
+        var defaultTrackArray = data.tracks.items;
+        for (let i = 0; i < defaultTrackArray.length; i++) {
+            console.log('\nArtist: ' + defaultTrackArray[i].artists[0].name + '\n');
+            console.log('Track Name: ' + defaultTrackArray[i].name + '\n');
+            console.log('Track Preview: ' + defaultTrackArray[i].preview_url + '\n');
+            console.log('Album: ' + defaultTrackArray[i].album.name + '\n');
+            console.log('___________________________________________________________________________________' + '\n')
+            debugger
+        }
+    });
+}
 
 function movie() {
+
+    console.log('\n'+"Loading..."+'\n');
+
     //Omdi or movies request
     // We then run the request module on a URL with a JSON
-    request("http://www.omdbapi.com/?t="+input+"&y=&plot=short&apikey=trilogy", function (error, response, body) {
+    // movies = thirdArgument();
+
+    request("http://www.omdbapi.com/?t=" + argument + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
 
         var rating = JSON.parse(body).Ratings[1];
         // If there were no errors and the response code was 200 (i.e. the request was successful)...
@@ -82,12 +165,78 @@ function movie() {
             console.log(JSON.parse(body).Language);
             console.log(JSON.parse(body).Plot);
             console.log(JSON.parse(body).Actors);
-
+            debugger
         }
     });
 }
 
+function movieDefault() {
+
+    console.log('\n'+"Loading..."+'\n');
+
+    //Omdi or movies request
+    // We then run the request module on a URL with a JSON
+    // movies = thirdArgument();
+
+    request("http://www.omdbapi.com/?t=mr.nobody&y=&plot=short&apikey=trilogy", function (error, response, body) {
+
+        var rating = JSON.parse(body).Ratings[1];
+        // If there were no errors and the response code was 200 (i.e. the request was successful)...
+        if (!error && response.statusCode === 200) {
+
+            // Then we print out the imdbRating
+            console.log(JSON.parse(body).Title);
+            console.log(JSON.parse(body).Year);
+            console.log(Object.values(rating).join(": "));
+            console.log(JSON.parse(body).Country);
+            console.log(JSON.parse(body).Language);
+            console.log(JSON.parse(body).Plot);
+            console.log(JSON.parse(body).Actors);
+            debugger
+        }
+    });
+}
+
+function what() {
+
+
+
+    fs.readFile('random.txt', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+
+            var readArray = data.split(",");
+
+            condition = readArray[0];
+
+            argument = readArray[1];
+
+            spot();
+        }
+
+    });
+}
+
+
+
+function thirdArgument() {
+
+    // Stores all possible arguments in array.
+    argumentArray = process.argv;
+
+    // Loops through words in node argument.
+    for (var i = 3; i < argumentArray.length; i++) {
+        argument += argumentArray[i];
+    }
+    return argument;
+}
+
+
+
 function check() {
-    
+
     console.log("Check your spelling dude!");
 }
+
+main();
